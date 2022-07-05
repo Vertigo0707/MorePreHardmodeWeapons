590aadcb45565b73964ce22be1784c929257e0a9
Vertigo0707 <108311137+Vertigo0707@users.noreply.github.com>
2022-07-04 21:00:53 -0400

    Add files via upload

---
+++ Buff/ButterflyBuff.cs
@@ -0,0 +1,35 @@
+﻿using MorePreHardmodeweapons.ModdedPlayer;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace MorePreHardmodeweapons.Buff
+{
+	public class ButterflyBuff : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Butterfly's blessing");
+			Description.SetDefault("The Butterfly's will fight for you");
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			SimpleModPlayer modPlayer = player.GetModPlayer<SimpleModPlayer>();
+			if (player.ownedProjectileCounts[ModContent.ProjectileType<Items.Summon.ButterflySummon>()] > 0)
+			{
+				modPlayer.ButterflySummon = true;
+			}
+			if (!modPlayer.ButterflySummon)
+			{
+				player.DelBuff(buffIndex);
+				buffIndex--;
+			}
+			else
+			{
+				player.buffTime[buffIndex] = 18000;
+			}
+		}
+	}
+}
---
+++ Buff/SparkPotionBuff.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace MorePreHardmodeweapons.Buff
+{
+	public class SparkPotionBuff : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Spark Potion is Active");
+			Description.SetDefault("Increases defense, speed, and regen");
+			Main.buffNoSave[Type] = true;
+			Main.buffNoTimeDisplay[Type] = true;
+		}
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.lifeRegen += 4;
+			player.statDefense += 4;
+			player.accRunSpeed += 0.9f;
+		}
+	}
+}
